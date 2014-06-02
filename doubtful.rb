470d8675ee467bd155285228ef1fca7bfe092b2c
William Clifford <wclifford@crowdcompass.com>
2014-06-02 11:17:26 -0700

    started implementing doubtful method calls

--- doubtful.rb
+++ doubtful.rb
@@ -5,16 +5,30 @@ module Doubtful
     'Really',
   ]
 
-  def self.doubt(prompt)
+  DENIALS = [
+    'n',
+    'no',
+  ]
+
+  def self.doubt_input(prompt, denials=DENIALS[0])
+    denials << DENIALS[0] if denials.empty?
     print "#{prompt}? "
-    gets.chomp[0].downcase === 'n'
+    denials.member?(gets.chomp[0].downcase)
   end
 
-  def self.redoubt(*prompts)
+  def self.redoubt_input(*prompt)
     prompts << PROMPTS[0] if prompts.empty?
     prompts.cycle do |prompt| 
       return if doubt prompt
     end
+  end
+ 
+  def self.doubt_output(denial, &block)
+    denial == yield
+  end
+
+  def self.redoubt_output
+    raise NotImplementedError
   end
 
 end
